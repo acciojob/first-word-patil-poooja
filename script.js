d35debbccb8526b524cd36a91c83a44f0f7479c1
poojapatil <156627026+patil-poooja@users.noreply.github.com>
2025-01-19 14:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function firstWord(s) {
   // your code here
-	const word = s.split(" ")[0];
-	return word;
+	if(s.indexOf(" ")===-1){
+		return s;
+	};
+	else{
+		const word = s.split(" ")[0];
+		return word;
+	};
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
